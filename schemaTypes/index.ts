bdee99016f20d1388e9193145a0a6f69b9bcba31
Md. Sakib Hasan <sakib3046@gmail.com>
2024-06-05 18:55:58 +0000

    dfs

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,8 @@
 
-import header from "./schemas/header"
+import blogs from "./schemas/blogs";
+import header from "./schemas/header";
 import sites from "./schemas/sites";
 import sociallinks from "./schemas/sociallinks";
 
-export const schemaTypes = [header,sites,sociallinks]
+
+export const schemaTypes = [header,sites,sociallinks,blogs]
